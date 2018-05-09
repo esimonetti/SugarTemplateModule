2b912f476043149ba90fe1b48335159737aba2ca
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-05-09 11:40:06 +1000

    Added Ext

--- configuration/templates.php
+++ configuration/templates.php
@@ -1,6 +1,6 @@
 <?php
 $templates['custom_extension_modules'] = array(
-    'directory_pattern' => 'custom/Extension/modules/{MODULENAME}',
+    'directory_pattern' => 'custom/Extension/modules/{MODULENAME}/Ext',
     'modules' => array(
         //'Accounts' => 'Account',
     )
